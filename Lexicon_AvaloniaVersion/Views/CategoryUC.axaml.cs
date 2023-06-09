850b2d17e93c1466010a39fb12cab4ea5ee8b338
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-06-09 10:22:24 +0200

    Starting

---
+++ Lexicon_AvaloniaVersion/Views/CategoryUC.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lexicon_AvaloniaVersion.Views
+{
+    public partial class CategoryUC : UserControl
+    {
+        public CategoryUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lexicon_AvaloniaVersion/Views/EntryView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Lexicon_AvaloniaVersion.Views
+{
+    public partial class EntryView : UserControl
+    {
+        public EntryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
